8a5e1cb98a6767b7f422e190cb1aee465dc9afb1
DionatanLeao <dionatan.ti@outlook.com>
2024-06-05 22:59:16 -0300

    feat/Lesson: Custom HttpResponse

--- src/main/java/com/udemy/broker/quotes/QuotesRestApi.java
+++ src/main/java/com/udemy/broker/quotes/QuotesRestApi.java
@@ -2,6 +2,7 @@ package com.udemy.broker.quotes;
 
 import com.udemy.broker.assets.Asset;
 import com.udemy.broker.assets.AssetsRestApi;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import org.slf4j.Logger;
@@ -10,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class QuotesRestApi {
@@ -25,7 +27,19 @@ public class QuotesRestApi {
       final String assetParam = context.pathParam("asset");
       LOG.debug("Asset parameter: {}", assetParam);
 
-      var response = cachedQuotes.get(assetParam).toJsonObject();
+      var maybeQuotes = Optional.ofNullable(cachedQuotes.get(assetParam));
+      if(maybeQuotes.isEmpty()) {
+        context.response()
+          .setStatusCode(HttpResponseStatus.NOT_FOUND.code())
+          .end(new JsonObject()
+            .put("message", "quote for asset " + assetParam + " not available!")
+            .put("path", context.normalizedPath())
+            .toBuffer()
+          );
+        return;
+      }
+
+      final JsonObject response = maybeQuotes.get().toJsonObject();
       LOG.info("Path {} responds with {}", context.normalizedPath(), response.encode());
       context.response().end(response.toBuffer());
     });
