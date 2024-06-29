c6ddd5ccc28a49dc305ef1c1161ae02594486dde
DionatanLeao <dionatan.ti@outlook.com>
2024-06-29 14:08:10 -0300

    feat/Lesson: HTTP Routing - DELETE

--- src/main/java/com/udemy/broker/watchlist/WatchListRestApi.java
+++ src/main/java/com/udemy/broker/watchlist/WatchListRestApi.java
@@ -3,6 +3,7 @@ package com.udemy.broker.watchlist;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,8 +17,7 @@ public class WatchListRestApi {
     final HashMap<UUID, WatchList> watchListPerAccount = new HashMap<UUID, WatchList>();
     final String path = "/account/watchlist/:accountId";
       parent.get(path).handler(context -> {
-        String accountId = context.pathParam("accountId");
-        LOG.debug("{} for account {}", context.normalizedPath(), accountId);
+        String accountId = getAccountId(context);
         var watchList = Optional.ofNullable(watchListPerAccount.get(UUID.fromString(accountId)));
 
         if(watchList.isEmpty()) {
@@ -34,15 +34,23 @@ public class WatchListRestApi {
         context.response().end(watchList.get().toJsonObject().toBuffer());
       });
       parent.put(path).handler(context -> {
-        String accountId = context.pathParam("accountId");
-        LOG.debug("{} for account {}", context.normalizedPath(), accountId);
+        String accountId = getAccountId(context);
         var json = context.getBodyAsJson();
         var watchList = json.mapTo(WatchList.class);
         watchListPerAccount.put(UUID.fromString(accountId), watchList);
         context.response().end(json.toBuffer());
       });
       parent.delete(path).handler(context -> {
-
+        String accountId = getAccountId(context);
+        final WatchList deleted = watchListPerAccount.remove(UUID.fromString(accountId));
+        LOG.info("Deleted: {}, Remaining: {}", deleted, watchListPerAccount.values());
+        context.response().end(deleted.toJsonObject().toBuffer());
       });
+  }
+
+  private static String getAccountId(RoutingContext context) {
+    String accountId = context.pathParam("accountId");
+    LOG.debug("{} for account {}", context.normalizedPath(), accountId);
+    return accountId;
   }
 }
