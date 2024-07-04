62b751f37ee3c0f81ad5cd789612dfad9320ed66
DionatanLeao <dionatan.ti@outlook.com>
2024-07-04 16:39:34 -0300

    feat/Lesson: Refactor HTTP Handlers

--- src/main/java/com/udemy/broker/assets/AssetsRestApi.java
+++ src/main/java/com/udemy/broker/assets/AssetsRestApi.java
@@ -1,27 +1,14 @@
 package com.udemy.broker.assets;
 
-import io.netty.handler.codec.http.HttpHeaderValues;
-import io.vertx.core.http.HttpHeaders;
-import io.vertx.core.json.JsonArray;
 import io.vertx.ext.web.Router;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class AssetsRestApi {
   public static final List<String> ASSETS = Arrays.asList("AAPL", "AMZN", "FB", "GOOG", "MSFT", "NFLX", "TSLA");
-  private static final Logger LOG = LoggerFactory.getLogger(AssetsRestApi.class);
 
   public static void attach(Router parent) {
-    parent.get("/assets").handler(context -> {
-      final JsonArray response = new JsonArray();
-      ASSETS.stream().map(Asset::new).forEach(response::add);
-      LOG.info("Path {} responds with {}", context.normalizedPath(), response.encode());
-      context.response()
-        .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
-        .putHeader("my-header", "my-value")
-        .end(response.toBuffer());
-    });
+    parent.get("/assets").handler(new GetAssetsHandler());
   }
 }
