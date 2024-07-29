859fd8822d34d422fa360f04686c8f9b122d5eec
DionatanLeao <dionatan.ti@outlook.com>
2024-07-29 12:02:30 -0300

    feat/Lesson: SQL Template - Delete

--- src/main/java/com/udemy/broker/watchlist/WatchListRestApi.java
+++ src/main/java/com/udemy/broker/watchlist/WatchListRestApi.java
@@ -22,6 +22,7 @@ public class WatchListRestApi {
     final String pgPath = "/pg/account/watchlist/:accountId";
     parent.get(pgPath).handler(new GetWatchListFromDatabaseHandler(db));
     parent.put(pgPath).handler(new PutWatchListFromDatabaseHandler(db));
+    parent.delete(pgPath).handler(new DeleteWatchListFromDatabaseHandler(db));
   }
 
   static String getAccountId(RoutingContext context) {
